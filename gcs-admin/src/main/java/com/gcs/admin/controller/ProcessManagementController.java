967f312bc71b5486ed5c87d0ca1b3f26ceba0a29
liyicheng1 <21620336@qq.com>
2023-06-19 15:05:00 +0800

    添加了流程管理表的页面
    自带生成的增删查改，还需要类型对应的数据字典的设计，查询表单应该对布局进行优化（好像没什么大不了）

---
+++ gcs-admin/src/main/java/com/gcs/admin/controller/ProcessManagementController.java
@@ -0,0 +1,104 @@
+package com.gcs.admin.controller;
+
+import java.util.List;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.gcs.common.annotation.Log;
+import com.gcs.common.core.controller.BaseController;
+import com.gcs.common.core.domain.AjaxResult;
+import com.gcs.common.enums.BusinessType;
+import com.gcs.admin.domain.ProcessManagement;
+import com.gcs.admin.service.IProcessManagementService;
+import com.gcs.common.utils.poi.ExcelUtil;
+import com.gcs.common.core.page.TableDataInfo;
+
+/**
+ * 流程管理Controller
+ * 
+ * @author xm06-gcs
+ * @date 2023-06-19
+ */
+@RestController
+@RequestMapping("/process/process_management")
+public class ProcessManagementController extends BaseController
+{
+    @Autowired
+    private IProcessManagementService processManagementService;
+
+    /**
+     * 查询流程管理列表
+     */
+    @PreAuthorize("@ss.hasPermi('process:process_management:list')")
+    @GetMapping("/list")
+    public TableDataInfo list(ProcessManagement processManagement)
+    {
+        startPage();
+        List<ProcessManagement> list = processManagementService.selectProcessManagementList(processManagement);
+        return getDataTable(list);
+    }
+
+    /**
+     * 导出流程管理列表
+     */
+    @PreAuthorize("@ss.hasPermi('process:process_management:export')")
+    @Log(title = "流程管理", businessType = BusinessType.EXPORT)
+    @PostMapping("/export")
+    public void export(HttpServletResponse response, ProcessManagement processManagement)
+    {
+        List<ProcessManagement> list = processManagementService.selectProcessManagementList(processManagement);
+        ExcelUtil<ProcessManagement> util = new ExcelUtil<ProcessManagement>(ProcessManagement.class);
+        util.exportExcel(response, list, "流程管理数据");
+    }
+
+    /**
+     * 获取流程管理详细信息
+     */
+    @PreAuthorize("@ss.hasPermi('process:process_management:query')")
+    @GetMapping(value = "/{id}")
+    public AjaxResult getInfo(@PathVariable("id") Long id)
+    {
+        return success(processManagementService.selectProcessManagementById(id));
+    }
+
+    /**
+     * 新增流程管理
+     */
+    @PreAuthorize("@ss.hasPermi('process:process_management:add')")
+    @Log(title = "流程管理", businessType = BusinessType.INSERT)
+    @PostMapping
+    public AjaxResult add(@RequestBody ProcessManagement processManagement)
+    {
+        return toAjax(processManagementService.insertProcessManagement(processManagement));
+    }
+
+    /**
+     * 修改流程管理
+     */
+    @PreAuthorize("@ss.hasPermi('process:process_management:edit')")
+    @Log(title = "流程管理", businessType = BusinessType.UPDATE)
+    @PutMapping
+    public AjaxResult edit(@RequestBody ProcessManagement processManagement)
+    {
+        return toAjax(processManagementService.updateProcessManagement(processManagement));
+    }
+
+    /**
+     * 删除流程管理
+     */
+    @PreAuthorize("@ss.hasPermi('process:process_management:remove')")
+    @Log(title = "流程管理", businessType = BusinessType.DELETE)
+	@DeleteMapping("/{ids}")
+    public AjaxResult remove(@PathVariable Long[] ids)
+    {
+        return toAjax(processManagementService.deleteProcessManagementByIds(ids));
+    }
+}
