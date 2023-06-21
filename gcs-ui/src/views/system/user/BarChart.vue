badaf0a127ae8e3952c35edfb6131091b958f6ae
laybxc <1297137854@qq.com>
2023-06-21 11:16:16 +0800

    修复了前端的问题2

--- gcs-ui/src/views/system/user/BarChart.vue
+++ gcs-ui/src/views/system/user/BarChart.vue
@@ -3,102 +3,93 @@
 </template>
 
 <script>
-import * as echarts from 'echarts';
-require('echarts/theme/macarons')// echarts theme
-import resize from '@/views/dashboard/mixins/resize'
-import {getStatistics} from "@/api/system/user"
-const animationDuration = 6000
+  import * as echarts from 'echarts';
+  require('echarts/theme/macarons')// echarts theme
+  import resize from '@/views/dashboard/mixins/resize'
+  import {getStatistics} from "@/api/system/user"
+  const animationDuration = 6000
 
-export default {
-  mixins: [resize],
-  props: {
-    className: {
-      type: String,
-      default: 'chart'
+  export default {
+    mixins: [resize],
+    props: {
+      className: {
+        type: String,
+        default: 'chart'
+      },
+      width: {
+        type: String,
+        default: '100%'
+      },
+      height: {
+        type: String,
+        default: '300px'
+      },
     },
-    width: {
-      type: String,
-      default: '100%'
+    data() {
+      return {
+        chart: null,
+        dept : [],
+        num : [],
+      }
     },
-    height: {
-      type: String,
-      default: '300px'
-    },
-
-  },
-  data() {
-    return {
-      chart: null,
-      dept : [],
-      num : [],
-    }
-  },
-  mounted() {
-
-    this.$nextTick(() => {
-      this.initChart()
-    })
-  },
-  beforeDestroy() {
-    if (!this.chart) {
-      return
-    }
-    this.chart.dispose()
-    this.chart = null
-  },
-
-  methods: {
-
-    initChart() {
-      this.chart = echarts.init(this.$el, 'macarons');
-
-      getStatistics().then(response => {
-        console.log(response);
-        this.dept = JSON.parse(JSON.stringify(response.dept));
-        this.num = JSON.parse(JSON.stringify(response.num));
-
-        console.log(this.dept);
-        console.log(this.num);
-      });
-      this.chart.setOption({
-
-        tooltip: {
-          trigger: 'axis',
-          axisPointer: { // 坐标轴指示器，坐标轴触发有效
-            type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
-          }
-        },
-        grid: {
-          top: 10,
-          left: '2%',
-          right: '2%',
-          bottom: '3%',
-          containLabel: true
-        },
-        xAxis: [{
-          type: 'category',
-          data: this.dept,
-          axisTick: {
-            alignWithLabel: true
-          }
-        }],
-        yAxis: [{
-          type: 'value',
-          axisTick: {
-            show: false
-          }
-        }],
-        series: [{
-          name: '人数',
-          type: 'bar',
-          stack: 'vistors',
-          barWidth: '60%',
-          data: this.num,
-          animationDuration
-        }],
+    mounted() {
+      this.$nextTick(() => {
+        this.initChart()
       })
+    },
+    beforeDestroy() {
+      if (!this.chart) {
+        return
+      }
+      this.chart.dispose()
+      this.chart = null
+    },
+    methods: {
+      initChart() {
+        this.chart = echarts.init(this.$el, 'macarons');
 
+        getStatistics().then(response => {
+          this.dept = JSON.parse(JSON.stringify(response.dept));
+          this.num = JSON.parse(JSON.stringify(response.num));
+
+          this.chart.setOption({
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: { // 坐标轴指示器，坐标轴触发有效
+                type: 'shadow' // 默认为直线，可选为：'line' | 'shadow'
+              }
+            },
+            grid: {
+              top: 10,
+              left: '2%',
+              right: '2%',
+              bottom: '3%',
+              containLabel: true
+            },
+            xAxis: [{
+              type: 'category',
+              data: this.dept,
+              axisTick: {
+                alignWithLabel: true
+              }
+            }],
+            yAxis: [{
+              type: 'value',
+              axisTick: {
+                show: false
+              }
+            }],
+            series: [{
+              name: '人数',
+              type: 'bar',
+              stack: 'vistors',
+              barWidth: '60%',
+              data: this.num,
+              animationDuration
+            }],
+          });
+        });
+      }
     }
   }
-}
 </script>
